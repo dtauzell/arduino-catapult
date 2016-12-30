6043cc85b249b5ec5f5226ba6cd796f0335a1aa5
David Tauzell <dave@tauzell.org>
2016-12-30 14:01:35 -0600

    Added buzzer and distance sensor

--- Catapult/Catapult.ino
+++ Catapult/Catapult.ino
@@ -1,33 +1,59 @@
 
 #include <Servo.h>
+#include <NewPing.h>
+
+#define TRIGGER_PIN  8
+#define ECHO_PIN     7
+#define MAX_DISTANCE 200
+#define FIRE_AT_MIN  65
+#define FIRE_AT_MAX   80
+#define BUZZER_PIN 12
 
 const int LeverPin = 10;
 const int TriggerPin = 13;
 
 const int leverLoose = 130;
 const int leverTight = 20;
+const int leverTense = 100;
 
-const int triggerClosed = 10;
+const int triggerClosed = 5;
 const int triggerOpen = 90;
+
+
+
 
 Servo leverServo;
 Servo triggerServo;
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup() {
   Serial.begin(9600);
   leverServo.attach(LeverPin);
   triggerServo.attach(TriggerPin);
-
+  pinMode(BUZZER_PIN, OUTPUT);
   prepare();  
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(250);
+  digitalWrite(BUZZER_PIN, LOW);
 }
 
 int times = 0;
 void loop() {
-  if( times++ < 10) {
+  int cm = sonar.ping_cm();
+  Serial.print("Ping: ");
+  Serial.print(cm);
+  Serial.println("cm");
+  digitalWrite(BUZZER_PIN, LOW);
+  if( cm >= FIRE_AT_MIN && cm <= FIRE_AT_MAX ){  
     fire();
     prepare();
     delay(2000);
   }
+  else if( cm > FIRE_AT_MAX && cm < FIRE_AT_MAX+150){
+    digitalWrite(BUZZER_PIN, HIGH);
+  }
+  delay(100);
 }
 
 void prepare() {
@@ -35,6 +61,8 @@ void prepare() {
   delay(2000);
   closeTrigger();
   delay(2000);
+  tensionLever();
+  delay(1000);
 }
 
 void fire() {
@@ -54,6 +82,10 @@ void closeTrigger() {
 
 void armLever() {
   leverServo.write(leverTight);
+}
+
+void tensionLever() {
+  leverServo.write(leverTense);
 }
 
 void loosenLever() {
